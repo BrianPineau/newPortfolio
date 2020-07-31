073b17af26fd7bb6856724f423bd644a44c765a7
Brian Pineau <pinoub@gmail.com>
2020-07-31 07:10:49 -0500

    Finished layout for projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, CardText, IconButton } from 'react-mdl';
 
 class Projects extends Component {
   constructor(props) {
@@ -10,8 +10,63 @@ class Projects extends Component {
   //Select the category
   toggleCategories() {
     if(this.state.activeTab === 0) {
-    return (
-        <div><h1>This is JavaScript</h1></div>
+      return (
+
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover' }} >React Project #1</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry standard
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover' }} >React Project #2</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry standard
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://reactjs.org/logo-og.png) center / cover' }} >React Project #3</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry standard
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: '#fff' }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+        
+      )
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div><h1>This is React</h1></div>
+      )
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div><h1>This is NodeJS</h1></div>
       )
     }
   }
@@ -26,9 +81,13 @@ class Projects extends Component {
           <Tab>NodeJS</Tab>
         </Tabs>
 
-        <section className="projects-grid">
-          {this.toggleCategories()}
-        </section>
+        
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+
       </div>
     )
   }

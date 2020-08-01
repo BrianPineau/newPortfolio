81b5d3f77dc24d75f17a8200b2e34eaeba5ae926
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2020-07-31 20:55:27 -0500

    Created contact card

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
   render() {
     return (
-      <div><h1>Contact Page</h1></div>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Brian Pineau</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: '250px' }}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500's, when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
+          </Cell>
+          <Cell col={6}>half page</Cell>
+        </Grid>
+      </div>
     )
   }
 }

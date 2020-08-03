9f107ce1f4cb40ac1251a5cef5b8fc818f182d96
CommanderBrian <40133130+BrianPineau@users.noreply.github.com>
2020-08-03 14:51:20 -0500

    Finished contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render() {
@@ -15,7 +15,33 @@ class Contact extends Component {
             />
             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500's, when an unknown printer took a gallery of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
           </Cell>
-          <Cell col={6}>half page</Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    (501) 701-7890
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    pinoub@gmail.com
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-skype" aria-hidden="true"/>
+                    live:.cid.b9e99b6969a02cf2
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
         </Grid>
       </div>
     )
